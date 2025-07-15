b98a45670d03c16d95fa0b180eb082680be02e7e
Waqas-Khan-CodeCanvas <waqaskhan11411704@gmail.com>
2025-07-15 23:32:45 +0500

    useContext

--- 07reactRouter/src/components/index.js
+++ 07reactRouter/src/components/index.js
@@ -1,20 +1,20 @@
 
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as Home } from './Home';
-export { default as About } from './About';
-export { default as Contact } from './Contact';
-export { default as Login } from './Login';
-export { default as SignUp } from './Signup';
+// export { default as Header } from './Header';
+// export { default as Footer } from './Footer';
+// export { default as Home } from './Home';
+// export { default as About } from './About';
+// export { default as Contact } from './Contact';
+// export { default as Login } from './Login';
+// export { default as SignUp } from './Signup';
 
 // components/index.js
-// import { lazy } from 'react';
+import { lazy } from 'react';
 
-// export const Header = lazy(() => import('./Header'));
-// export const Footer = lazy(() => import('./Footer'));
-// export const Home = lazy(() => import('./Home'));
-// export const About = lazy(() => import('./About'));
-// export const Contact = lazy(() => import('./Contact'));
-// export const Login = lazy(() => import('./Login'));
-// export const SignUp = lazy(() => import('./Signup'));
-// export const Layout = lazy(() => import('./Layout'));
+export const Header = lazy(() => import('./Header'));
+export const Footer = lazy(() => import('./Footer'));
+export const Home = lazy(() => import('./Home'));
+export const About = lazy(() => import('./About'));
+export const Contact = lazy(() => import('./Contact'));
+export const Login = lazy(() => import('./Login'));
+export const SignUp = lazy(() => import('./Signup'));
+export const Layout = lazy(() => import('./Layout'));
